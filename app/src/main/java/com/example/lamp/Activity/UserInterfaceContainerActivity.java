f36b984c399cba53ce7a706c3c19ff64a739a7b7
Shakil <sadakatulshakil94@gmail.com>
2020-09-03 18:34:23 +0600

    Code for home acitivity show products operation by Shakil

---
+++ app/src/main/java/com/example/lamp/Activity/UserInterfaceContainerActivity.java
@@ -0,0 +1,16 @@
+package com.example.lamp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lamp.R;
+
+public class UserInterfaceContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface_container);
+    }
+}
---
+++ app/src/main/java/com/example/lamp/Fragment/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.lamp.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lamp.R;
+
+public class HomeFragment extends Fragment {
+
+    private Context context;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
